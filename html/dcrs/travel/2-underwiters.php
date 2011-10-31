29e0c906396d326c15569176883a267759d0b5e6
unknown <tb@yellowgrey.com>
2011-10-31 15:08:20 +0000

    Cleanup

--- html/dcrs/travel/2-underwiters.php
+++ html/dcrs/travel/2-underwiters.php
@@ -7,18 +7,18 @@
 <h3>My policy number starts with "TT"</h3>
 <div class="rmc">
 <p>Your policy is underwritten by Ageas Insurance Limited</p>
-<h5 class="split">Brand Travel Insurance</h5>
-<h5 class="split">Purchased online</h5>
+<h5>Brand Travel Insurance</h5>
+<h5>Purchased online</h5>
 <ul class="docs">
 <li><a shape="rect" rel="external" href="/assets/sections/travelins/pdf/brand-travel-policy-summary-online.pdf">Policy Summary</a> [PDF, 99KB]</li>
 <li><a shape="rect" rel="external" href="/assets/sections/travelins/pdf/brand-travel-policy-booklet-online.pdf">Policy Booklet</a> [PDF, 383KB]</li>
 </ul>
-<h5 class="split">Purchased by Phone</h5>
+<h5>Purchased by Phone</h5>
 <ul class="docs">
 <li><a shape="rect" rel="external" href="/assets/sections/travelins/pdf/brand-travel-policy-summary-phone.pdf">Policy Summary</a> [PDF, 99KB]</li>
 <li><a shape="rect" rel="external" href="/assets/sections/travelins/pdf/brand-travel-policy-booklet-phone.pdf">Policy Booklet</a> [PDF, 383KB]</li>
 </ul>
-<h5 class="split">Purchased in-store</h5>
+<h5>Purchased in-store</h5>
 <ul class="docs">
 <li><a shape="rect" rel="external" href="/assets/sections/travelins/pdf/brand-travel-policy-summary-instore.pdf">Policy Summary</a> [PDF, 64KB]</li>
 <li><a shape="rect" rel="external" href="/assets/sections/travelins/pdf/brand-travel-policy-booklet-instore.pdf">Policy Booklet</a> [PDF, 259KB]</li>
@@ -34,17 +34,17 @@
 <h3>My policy number starts with a number</h3>
 <div class="rmc">
 <p>Your policy is underwritten by UK Insurance Limited</p>
-<h5 class="split">Purchased online &amp; in-store - Annual Policy</h5>
+<h5>Purchased online &amp; in-store - Annual Policy</h5>
 <ul class="docs">
 <li><a shape="rect" rel="external" href="/assets/sections/travelins/pdf/brand-travel-insurance-annual-policy-summary.pdf">Annual Trip Policy Summary</a> [PDF, 349KB]</li>
 <li><a shape="rect" rel="external" href="/assets/sections/travelins/pdf/brand-travel-insurance-annual-policy-document.pdf">Annual Trip Policy Booklet</a> [PDF, 282KB]</li>
 </ul>
-<h5 class="split">Purchased online &amp; in-store - Single Policy</h5>
+<h5>Purchased online &amp; in-store - Single Policy</h5>
 <ul class="docs">
 <li><a shape="rect" rel="external" href="/assets/sections/travelins/pdf/brand-travel-insurance-single-policy-summary.pdf">Single Trip Policy Summary</a> [PDF, 349KB]</li>
 <li><a shape="rect" rel="external" href="/assets/sections/travelins/pdf/brand-travel-insurance-single-policy-document.pdf">Single Trip Policy Document</a> [PDF, 282KB]</li>
 </ul>
-<h5 class="split">Purchased by Phone</h5>
+<h5>Purchased by Phone</h5>
 <ul class="docs">
 <li><a shape="rect" rel="external" href="/assets/sections/travelins/pdf/brand-travel-insurance-policy-summary-phone.pdf">Policy Summary</a> [PDF, 353KB]</li>
 <li><a shape="rect" rel="external" href="/assets/sections/travelins/pdf/brand-travel-insurance-policy-document-phone.pdf">Policy Document</a> [PDF, 304KB]</li>
