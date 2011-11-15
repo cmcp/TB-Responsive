324b405051ad1e576918a56e9e4e37bedc74d3d0
unknown <tb@yellowgrey.com>
2011-11-15 10:02:16 +0000

    Added full layouts samples.

---
+++ html/dcrs/layout/left-nav-content.php
@@ -0,0 +1,20 @@
+﻿  <div class="panel type-1 bdr-1 bg cnr nav">
+    <div class="wrap">
+      <ul id="nav-secondary">
+        <li class="active">Section homepage
+
+          <ul class="sub-level">
+            <li class=""><a target="" shape="rect" href="/layouts/content/1">Canvas 1 (Fireplace)</a></li>
+            <li class=""><a target="" shape="rect" href="/layouts/content/2">Canvas 2 (50:50 / 50:50)</a></li>
+            <li class=""><a target="" shape="rect" href="/layouts/content/3">Canvas 3 (100 / Fireplace)</a></li>
+            <li class=""><a target="" shape="rect" href="/layouts/content/4">Canvas 4 (50:50 / Fireplace)</a></li>
+            <li class=""><a target="" shape="rect" href="/layouts/content/5">Canvas 5 (100 / 50:50)</a></li>
+            <li class=""><a target="" shape="rect" href="/layouts/content/6">Canvas 6 (100 / 50:50 / Fireplace)</a></li>
+            <li class=""><a target="" shape="rect" href="/layouts/content/7">Canvas 7 (100 / 50:50 / Fireplace)</a></li>
+            <li class=""><a target="" shape="rect" href="/layouts/content/8">Canvas 8 (100 / 50:50 / 100)</a></li>
+            <li class=""><a target="" shape="rect" href="/layouts/content/9">Canvas 9 (100)</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
---
+++ html/dcrs/layout/left-nav-nav.php
@@ -0,0 +1,20 @@
+﻿  <div class="panel type-1 bdr-1 bg cnr nav">
+    <div class="wrap">
+      <ul id="nav-secondary">
+        <li class="active">Section homepage
+
+          <ul class="sub-level">
+            <li class=""><a target="" shape="rect" href="/layouts/nav/1">Canvas 1 (Fireplace)</a></li>
+            <li class=""><a target="" shape="rect" href="/layouts/nav/2">Canvas 2 (50:50 / 50:50)</a></li>
+            <li class=""><a target="" shape="rect" href="/layouts/nav/3">Canvas 3 (100 / Fireplace)</a></li>
+            <li class=""><a target="" shape="rect" href="/layouts/nav/4">Canvas 4 (50:50 / Fireplace)</a></li>
+            <li class=""><a target="" shape="rect" href="/layouts/nav/5">Canvas 5 (100 / 50:50)</a></li>
+            <li class=""><a target="" shape="rect" href="/layouts/nav/6">Canvas 6 (100 / 50:50 / Fireplace)</a></li>
+            <li class=""><a target="" shape="rect" href="/layouts/nav/7">Canvas 7 (100 / 50:50 / Fireplace)</a></li>
+            <li class=""><a target="" shape="rect" href="/layouts/nav/8">Canvas 8 (100 / 50:50 / 100)</a></li>
+            <li class=""><a target="" shape="rect" href="/layouts/nav/9">Canvas 9 (100)</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
