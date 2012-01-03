9bb18f6bc79ae9821131968a126a8ddd0fbd0aa6
Craig McPheat <inbox@craigmcpheat.com>
2012-01-03 23:38:52 +0000

    margin right on main nav, font size on register, first pass at button styles (todo: triple mixin gradients), white keyline on main nav anchors, margin on login, change .wrap width 100% to width auto, apply touch classes only to buttons in #maincontent, new button class login, upload new logo.

--- html/csso.php
+++ html/csso.php
@@ -9,7 +9,7 @@
   <div id="login">
     <label for="csso-login">Online Banking</label>
     <select id="csso-login" name="csso-login" tabindex="30"><option value="" selected>Select product...</option><option value="s">Savings</option><option value="l">Loans</option><option value="cc">Credit Cards</option><option value="bcc">Business Credit Cards</option><option value="mort">Mortgages</option></select>
-    <a href="#" class="btn btn-cta" tabindex="30">Login</a>
+    <a href="#" class="btn btn-login" tabindex="30">Login</a>
   </div>
   <div class="register">Not registered for Online Banking? <a href="#" tabindex="30">Register Now</a></div>
 </div><!-- .csso -->
