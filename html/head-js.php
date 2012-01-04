60d7541b3932ef70c2fc81aab2f1a3c72205c437
unknown <tb@yellowgrey.com>
2012-01-04 14:18:29 +0000

    layout and full site JS

--- html/head-js.php
+++ html/head-js.php
@@ -31,7 +31,7 @@ docCookies = {
 };
 (function () {
   if (docCookies.getItem('TB_fullsite') === 'true') {
-    document.getElementsByTagName('body')[0].className += ' wide';
+    document.getElementsByTagName('html')[0].className += ' wide';
     docCookies.setItem('screen', 'wide');
   }
   if (document.documentElement .clientWidth > 600) {
