b72d7a2192c3b477e290c64c9678e19d03429b40
Craig McPheat <inbox@craigmcpheat.com>
2012-01-05 00:27:19 +0000

    add contact tel number on signposts, defaqto chat on travel insurance homepage.

--- html/dcrs/insurance/travelins/1-call-for-quote.php
+++ html/dcrs/insurance/travelins/1-call-for-quote.php
@@ -1,4 +1,4 @@
-<div class="panel type-1 bdr-1 cnr bg  cta-left"><div class="wrap"><h3>Calling for a quote</h3><p>Want to talk to one of our team?</p>
-<ul class="cta">
-<li><a href="/personal/finance/insurance/travelins/contact.html" class="btn fom">Contact us</a></li>
-</ul></div></div>
+<div class="panel type-1 bdr-1 cnr bg  cta-left"><div class="wrap"><h3>Call us for a quote</h3><p style="font-size:22px;color:red">0845 293 9474</p>
+<p class="small">Lines open monday to Friday 8am - 8pm, Saturday 9am - 4pm, and Sunday 10am - 5pm. Calls may be recorded.</p>
+<a href="/personal/finance/insurance/travelins/contact.html" class="btn fom">Contact us</a>
+</div></div>
--- html/dcrs/insurance/travelins/2-what-do-i-get.php
+++ html/dcrs/insurance/travelins/2-what-do-i-get.php
@@ -9,3 +9,14 @@
 <p class="small">Please note excesses, terms and conditions apply. This is a summary of cover and you should refer to the <a href="#">policy documents</a> for full info.</p>
 </div>
 </div></div>
+<div class="panel type-2 bdr-2 cnr bg copy cta-left"><div class="wrap"><h3>Our cover levels are Defaqto related...</h3><div class="content">
+<p>All three of our cover levels have achieved star ratings from Defaqto, a leading UK independant financial research company.</p>
+<ul class="tick">
+<li><strong>Finest cover achieved a 5 star Defaqto Rating</strong>with an excellent range of features and benefits!</li>
+<li><strong>Standard cover level achieved a 3 star Defaqto Rating</strong> showing it's got good levels of travel insurance cover.</li>
+<li><strong>Value cover level has a 1 star Defaqto Rating</strong> even though it's our basic, no-frillstravel insurance.</li>
+</ul>
+<a href="#">Compare our three travel insurance cover levels...</a>
+</div>
+</div></div>
+<a href="#" style="float:right" class="btn btn-cta">Get Quote</a>
